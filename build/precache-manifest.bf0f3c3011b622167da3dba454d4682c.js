3bc392e6e2012015fe50d3efda5b7344cadae03c
Ayush Kulshrestha <akcool.ayush@gmail.com>
2021-04-24 19:06:17 +0530

    manifest update

---
+++ build/precache-manifest.bf0f3c3011b622167da3dba454d4682c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83673d28e1c74543286f72cd0ffe187e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86728b9e927ff821c72b",
+    "url": "/static/css/2.3c234385.chunk.css"
+  },
+  {
+    "revision": "736f828ee17db36e8a4b",
+    "url": "/static/css/main.0c48700b.chunk.css"
+  },
+  {
+    "revision": "86728b9e927ff821c72b",
+    "url": "/static/js/2.c632c792.chunk.js"
+  },
+  {
+    "revision": "736f828ee17db36e8a4b",
+    "url": "/static/js/main.9a1a6be3.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  }
+]);
