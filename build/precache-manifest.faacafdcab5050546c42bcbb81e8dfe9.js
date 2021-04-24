edacca00682575a76cbcb8586d7e9f5f624deaf4
shivanshmehendiratta <shivansh.19521@sscbs.du.ac.in>
2021-04-25 02:14:59 +0530

    cms-build

---
+++ build/precache-manifest.faacafdcab5050546c42bcbb81e8dfe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7757c932e8f7fcad3fdc87b47fc7d91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbb8fc1b48bdd6220115",
+    "url": "/static/css/2.53d2c191.chunk.css"
+  },
+  {
+    "revision": "addeb66218addaebf45d",
+    "url": "/static/css/main.8d597fe8.chunk.css"
+  },
+  {
+    "revision": "fbb8fc1b48bdd6220115",
+    "url": "/static/js/2.0595f8ae.chunk.js"
+  },
+  {
+    "revision": "addeb66218addaebf45d",
+    "url": "/static/js/main.566d245f.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  }
+]);
