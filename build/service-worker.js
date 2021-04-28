6fe798c645700873355d677d1851943eaede5925
Kajal Bobal <kajal@leewayhertz.com>
2021-04-28 15:34:11 +0530

    cms-build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ff67fcf7b64772f392f94974d5369aa.js"
+  "/precache-manifest.4ce53bef196adbcf265cfd480131ea59.js"
 );
 
 self.addEventListener('message', (event) => {
