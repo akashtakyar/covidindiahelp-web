4359c10ff0d9a8682ae2db16bfaa7e3c6116625a
sunnyLeeway <sunnyk@leewayhertz.com>
2021-05-03 22:32:34 +0530

    build for showing comment in detail

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c92d31be8d251da9a7d7ab0674a07592.js"
+  "/precache-manifest.fbe3464f16edf2097963421aae9a1583.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
