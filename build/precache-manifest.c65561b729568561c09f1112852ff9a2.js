26916610be01e8374ee4910d442d81b5546b8014
anuragLHT <anurag@leewayhertz.com>
2021-05-14 19:24:53 +0530

    custom comment field added

---
+++ build/precache-manifest.c65561b729568561c09f1112852ff9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db149e70a4515cad979a1585ad8faa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72587e0ded4c28d5b155",
+    "url": "/static/css/2.1158e52c.chunk.css"
+  },
+  {
+    "revision": "24bf1b6ce5e75149f015",
+    "url": "/static/css/main.59283404.chunk.css"
+  },
+  {
+    "revision": "72587e0ded4c28d5b155",
+    "url": "/static/js/2.0acc800a.chunk.js"
+  },
+  {
+    "revision": "a9a8623bafadc6a881c94b7cda41b655",
+    "url": "/static/js/2.0acc800a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bf1b6ce5e75149f015",
+    "url": "/static/js/main.133df1ac.chunk.js"
+  },
+  {
+    "revision": "c4cda4214c0c300269f1",
+    "url": "/static/js/runtime-main.d5bfd598.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7103709632c41a837aae9da51da1c93d.js"
+  "/precache-manifest.c65561b729568561c09f1112852ff9a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
