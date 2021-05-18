9e0b74557f63067d87a8374526ee28b376e8fa65
praveenk <praveenk@leewayhertz.com>
2021-05-18 11:33:59 +0530

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70d2aceb66e2bb917f91a1b124eaddcc.js"
+  "/precache-manifest.80557a2de1d8752dd4dbbd76e5fdf17c.js"
 );
 
 self.addEventListener('message', (event) => {
